3652ffc7a6d5596876bd6d4ac2792aa0208798f0
patric5267 <115068229+patric5267@users.noreply.github.com>
2024-02-08 20:07:04 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import All from './All'
+import { useNavigate } from 'react-router-dom'
+import { useSelector , useDispatch } from 'react-redux'
+import { getuser } from '../redux/action'
+import Navbar from './Navbar'
+import Phone from './Phone'
+const Home = () => {
+  const dispatch = useDispatch()
+  const {user , notes , isloading} = useSelector((state)=>state.auth)
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!localStorage.getItem("id")) {
+      navigate('/login')
+    }
+    else{
+       dispatch(getuser(localStorage.getItem("id")))
+    }
+  }, [])
+  if(isloading){
+    return <div className=' fixed h-full w-full  left-0 top-0 flex justify-center items-start'><span className="mt-[13rem] loader2"></span></div>
+  }
+  else{
+    return (
+      <>
+      <Navbar/>
+      <Phone/>
+     {user &&<div className='md:ml-2 w-full  h-full'>
+        <All a={location.pathname} notesdata={notes}/>
+      </div> }
+      </>
+    )
+  }
+ 
+}
+
+export default Home
